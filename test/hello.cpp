f978b97e4621b813f8e71243c376f84d6db79fdc
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2026-01-06 23:46:45 +0530

    Add endl to output in hello.cpp

--- test/hello.cpp
+++ test/hello.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 using namespace std;
+using std::cout;
+using std::endl;
 
 int main() {
-    cout << "Hello, Mehul Kumar Singh";
+    cout << "Hello, Mehul Kumar Singh"<<endl;
 }
