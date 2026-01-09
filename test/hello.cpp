39a87ac104655ea15e9af9c559d9435cf5095621
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2026-01-09 23:45:02 +0530

    Refactor main function with variable declarations
    Updated main function to include variable declarations and multiple output statements.

--- test/hello.cpp
+++ test/hello.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
 using namespace std;
-using std::cout;
-using std::endl;
 
-int main() {
-    cout << "Hello, Mehul Kumar Singh"<<endl;
+int main(){
+
+  int score;// variable declared
+  score = 110;
+
+  const int uid=1808;//it wont be abe to change as const is given. NOT MUTABLE!
+
+  int balance=500;
+  balance=1000;
+
+  cout << "Welcome to chai with CPP1" << endl ;
+
+  cout << "Welcome to chai with CPP2" << endl ;
+  cout << "Welcome to chai with CPP3" << endl ;
+  return 0;
 }
